f1088da865a97ac68afc1c5b80800751169d4431
Bo Liu <wy90021@Bos-Mac-mini.local>
2022-01-18 20:33:08 -0800

    initial test

---
+++ main.go
@@ -0,0 +1,43 @@
+// main.go
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+func hello() (string, error) {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"),
+	})
+
+	svc := s3.New(sess)
+
+	result, err := svc.ListBuckets(nil)
+
+	if err != nil {
+		exitErrorf("Unable to list buckets, %v", err)
+	}
+
+	log.Printf("Buckets:")
+
+	for _, b := range result.Buckets {
+		log.Printf("* %s created on %s\n", aws.StringValue(b.Name), aws.TimeValue(b.CreationDate))
+	}
+	return "Address: Performance", nil
+}
+
+func exitErrorf(msg string, args ...interface{}) {
+	log.Printf(msg + "\n")
+	os.Exit(1)
+}
+
+func main() {
+	// Make the handler available for Remove Procedure Call by AWS Lambda
+	lambda.Start(hello)
+}
