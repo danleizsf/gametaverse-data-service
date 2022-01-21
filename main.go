c226210ee7c8e0095d87232180eb4b6042c83a91
wy90021 <wy90021@Bos-Mac-mini.local>
2022-01-20 23:52:54 -0800

    update

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func hello() (string, error) {
 
 		for _, item := range resp.Contents {
 			log.Printf("file name: %s\n", *item.Key)
-			file, err := os.Create(string(*item.Key))
+			file, err := os.Create(string("bo"))
 			if err != nil {
 				exitErrorf("Unable to create tmp file, %v", err)
 			}
