9610af48139437e4434b35b8bd90d80a6247ef76
wy90021 <wy90021@Bos-Mac-mini.local>
2022-01-21 17:27:24 -0800

    update

--- main.go
+++ main.go
@@ -83,6 +83,7 @@ func hello() (string, error) {
 			transactions := converCsvStringToTransactionStructs(bodyString)
 			dateTimestamp, _ := strconv.Atoi(strings.Split(*item.Key, "-")[0])
 			dateString := time.Unix(int64(dateTimestamp), 0).UTC().Format("2006-January-01")
+			log.Printf(dateString)
 			daus[dateString] = getDau(transactions, int64(dateTimestamp))
 		}
 	}
