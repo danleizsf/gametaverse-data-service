0391cbbb3d6c38b9b6b0eef86c6020aa85e3863d
wy90021 <wy90021@gmail.com>
2022-01-31 19:47:23 -0800

    update

--- main.go
+++ main.go
@@ -194,7 +194,7 @@ func exitErrorf(msg string, args ...interface{}) {
 	os.Exit(1)
 }
 
-func getGameData() (string, error) {
+func getGameDau() map[string]int {
 	sess, _ := session.NewSession(&aws.Config{
 		Region: aws.String("us-west-1"),
 	})
@@ -209,7 +209,6 @@ func getGameData() (string, error) {
 
 	log.Printf("Buckets:")
 	daus := make(map[string]int)
-	dailyTransactionVolume := make(map[string]float64)
 
 	for _, bucket := range result.Buckets {
 		log.Printf("* %s created on %s\n", aws.StringValue(bucket.Name), aws.TimeValue(bucket.CreationDate))
@@ -244,10 +243,62 @@ func getGameData() (string, error) {
 			dateFormattedString := fmt.Sprintf("%d-%d-%d", dateObj.Year(), dateObj.Month(), dateObj.Day())
 			//daus[dateFormattedString] = getDauFromTransactions(transactions, int64(dateTimestamp))
 			daus[dateFormattedString] = getActiveUserNumFromTransfers(transfers, int64(dateTimestamp))
+		}
+	}
+	return daus
+}
+
+func getGameTransactionVolumes() map[string]float64 {
+	sess, _ := session.NewSession(&aws.Config{
+		Region: aws.String("us-west-1"),
+	})
+
+	svc := s3.New(sess)
+
+	result, err := svc.ListBuckets(nil)
+
+	if err != nil {
+		exitErrorf("Unable to list buckets, %v", err)
+	}
+
+	log.Printf("Buckets:")
+	dailyTransactionVolume := make(map[string]float64)
+
+	for _, bucket := range result.Buckets {
+		log.Printf("* %s created on %s\n", aws.StringValue(bucket.Name), aws.TimeValue(bucket.CreationDate))
+
+		resp, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(*bucket.Name)})
+		if err != nil {
+			exitErrorf("Unable to list object, %v", err)
+		}
+
+		for _, item := range resp.Contents {
+			log.Printf("file name: %s\n", *item.Key)
+			requestInput :=
+				&s3.GetObjectInput{
+					Bucket: aws.String(*bucket.Name),
+					Key:    aws.String(*item.Key),
+				}
+			result, err := svc.GetObject(requestInput)
+			if err != nil {
+				exitErrorf("Unable to get object, %v", err)
+			}
+			body, err := ioutil.ReadAll(result.Body)
+			if err != nil {
+				exitErrorf("Unable to get body, %v", err)
+			}
+			bodyString := fmt.Sprintf("%s", body)
+			//transactions := converCsvStringToTransactionStructs(bodyString)
+			transfers := converCsvStringToTransferStructs(bodyString)
+			log.Printf("transfer num: %d", len(transfers))
+			dateTimestamp, _ := strconv.Atoi(strings.Split(*item.Key, "-")[0])
+			//dateString := time.Unix(int64(dateTimestamp), 0).UTC().Format("2006-January-01")
+			dateObj := time.Unix(int64(dateTimestamp), 0).UTC()
+			dateFormattedString := fmt.Sprintf("%d-%d-%d", dateObj.Year(), dateObj.Month(), dateObj.Day())
 			dailyTransactionVolume[dateFormattedString] = getTransactionVolumeFromTransfers(transfers, int64(dateTimestamp))
 		}
 	}
-	return fmt.Sprintf("{starsharks: {dau: %v, dailyTransactionVolume: %v SEA Token}}", daus, dailyTransactionVolume), nil
+	return dailyTransactionVolume
 }
 
 func getUserData(address string) (string, error) {
@@ -306,8 +357,10 @@ func getUserData(address string) (string, error) {
 
 func process(ctx context.Context, input Input) (string, error) {
 	log.Printf("intput: %v", input)
-	if input.Method == "getGameData" {
-		return getGameData()
+	if input.Method == "getGameDaus" {
+		return fmt.Sprintf("{\"jsonrpc\":\"2.0\",\"result\":%v}", getGameDau()), nil
+	} else if input.Method == "getGameDailyTransactionVolumes" {
+		return fmt.Sprintf("{\"jsonrpc\":\"2.0\",\"result\":%v}", getGameTransactionVolumes()), nil
 	} else if input.Method == "getUserData" {
 		return getUserData(input.Data)
 	} else if input.Method == "getUserRetentionRate" {
