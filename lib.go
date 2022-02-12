1db4f0664dd9e72910487ab5f08270f5fd330111
wy90021 <wy90021@gmail.com>
2022-02-11 16:40:49 -0800

    update

--- lib.go
+++ lib.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
+	"sort"
 	"strconv"
 	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 func GetPerPayerType(perPayerTransfers map[string][]Transfer) map[string]payerType {
@@ -62,4 +72,255 @@ func ConvertCsvStringToTransferStructs(csvString string) []Transfer {
 		})
 	}
 	return transfers
+}
+
+func getActiveUsersFromTransfers(transfers []Transfer) map[string]bool {
+	uniqueAddresses := make(map[string]bool)
+	count := 0
+	for _, transfer := range transfers {
+		count += 1
+		uniqueAddresses[transfer.FromAddress] = true
+		uniqueAddresses[transfer.ToAddress] = true
+	}
+	return uniqueAddresses
+}
+
+func getUserTransactionVolume(address string, transfers []Transfer) float64 {
+	transactionVolume := float64(0)
+	for _, transfer := range transfers {
+		if transfer.FromAddress == address || transfer.ToAddress == address {
+			transactionVolume += transfer.Value
+			log.Printf("address: %s, transactionHash: %s, value: %v", address, transfer.TransactionHash, transfer.Value)
+		}
+	}
+	return transactionVolume / 1000000000000000000
+}
+
+func getTransactionVolumeFromTransfers(transfers []Transfer, timestamp int64) UserTransactionVolume {
+	renterTransactionVolume, purchaserTransactionVolume, withdrawerTransactionVolume := int64(0), int64(0), int64(0)
+	for _, transfer := range transfers {
+		if transfer.ContractAddress == starSharksRentContractAddresses {
+			renterTransactionVolume += int64(transfer.Value / float64(seaTokenUnit))
+		} else if transfer.ContractAddress == starSharksPurchaseContractAddresses || transfer.ContractAddress == starSharksAuctionContractAddresses {
+			purchaserTransactionVolume += int64(transfer.Value / float64(seaTokenUnit))
+		} else if transfer.ContractAddress == starSharksWithdrawContractAddresses {
+			withdrawerTransactionVolume += int64(transfer.Value / float64(seaTokenUnit))
+		}
+	}
+	return UserTransactionVolume{
+		RenterTransactionVolume:     renterTransactionVolume,
+		PurchaserTransactionVolume:  purchaserTransactionVolume,
+		WithdrawerTransactionVolume: withdrawerTransactionVolume,
+	}
+}
+
+func exitErrorf(msg string, args ...interface{}) {
+	log.Printf(msg + "\n")
+	os.Exit(1)
+}
+
+func getUserData(address string) (string, error) {
+	sess, _ := session.NewSession(&aws.Config{
+		Region: aws.String("us-west-1"),
+	})
+
+	svc := s3.New(sess)
+
+	dailyTransactionVolume := make(map[string]float64)
+
+	resp, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(dailyTransferBucketName)})
+	if err != nil {
+		exitErrorf("Unable to list object, %v", err)
+	}
+
+	for _, item := range resp.Contents {
+		log.Printf("file name: %s\n", *item.Key)
+		requestInput :=
+			&s3.GetObjectInput{
+				Bucket: aws.String(dailyTransferBucketName),
+				Key:    aws.String(*item.Key),
+			}
+		result, err := svc.GetObject(requestInput)
+		if err != nil {
+			exitErrorf("Unable to get object, %v", err)
+		}
+		body, err := ioutil.ReadAll(result.Body)
+		if err != nil {
+			exitErrorf("Unable to get body, %v", err)
+		}
+		bodyString := string(body)
+		//transactions := converCsvStringToTransactionStructs(bodyString)
+		transfers := ConvertCsvStringToTransferStructs(bodyString)
+		log.Printf("transfer num: %d", len(transfers))
+		dateTimestamp, _ := strconv.Atoi(strings.Split(*item.Key, "-")[0])
+		//dateString := time.Unix(int64(dateTimestamp), 0).UTC().Format("2006-January-01")
+		dateObj := time.Unix(int64(dateTimestamp), 0).UTC()
+		dateFormattedString := fmt.Sprintf("%d-%d-%d", dateObj.Year(), dateObj.Month(), dateObj.Day())
+		//daus[dateFormattedString] = getDauFromTransactions(transactions, int64(dateTimestamp))
+		dailyTransactionVolume[dateFormattedString] = getUserTransactionVolume(address, transfers)
+	}
+	return fmt.Sprintf("{starsharks: {dailyTransactionVolume: %v SEA Token}}", dailyTransactionVolume), nil
+}
+
+func getPerUserSpending(transfers []Transfer) map[string]int64 {
+	perUserSpending := make(map[string]int64)
+	for _, transfer := range transfers {
+		if _, ok := starSharksGameWalletAddresses[transfer.FromAddress]; ok {
+			continue
+		}
+		if spending, ok := perUserSpending[transfer.FromAddress]; ok {
+			perUserSpending[transfer.FromAddress] = spending + int64(transfer.Value/1000000000000000000)
+		} else {
+			perUserSpending[transfer.FromAddress] = int64(transfer.Value / 1000000000000000000)
+		}
+	}
+	return perUserSpending
+}
+
+func generateValueDistribution(perUserValue map[string]int64) []ValueFrequencyPercentage {
+	valueDistribution := make(map[int64]int64)
+	totalFrequency := int64(0)
+	for _, value := range perUserValue {
+		valueDistribution[value] += 1
+		totalFrequency += 1
+	}
+	valuePercentageDistribution := make(map[int64]float64)
+	for value, frequency := range valueDistribution {
+		valuePercentageDistribution[value] = float64(frequency) / float64(totalFrequency)
+	}
+	result := make([]ValueFrequencyPercentage, len(valuePercentageDistribution))
+	idx := 0
+	for value, percentage := range valuePercentageDistribution {
+		result[idx] = ValueFrequencyPercentage{
+			Value:               value,
+			FrequencyPercentage: percentage,
+		}
+		idx += 1
+	}
+	sort.Slice(result, func(i, j int) bool {
+		return result[i].Value < result[j].Value
+	})
+	return result
+}
+
+func isEligibleToProcess(timeObj time.Time, targetTimeObjs []time.Time) bool {
+	eligibleToProcess := false
+	for _, targetTimeObj := range targetTimeObjs {
+		log.Printf("targetTime: %v, time: %v", targetTimeObj, timeObj)
+		if targetTimeObj.Year() == timeObj.Year() && targetTimeObj.Month() == timeObj.Month() && targetTimeObj.Day() == timeObj.Day() {
+			eligibleToProcess = true
+			break
+		}
+	}
+	return eligibleToProcess
+}
+
+func generateTimeObjs(input Input) []time.Time {
+	times := make([]time.Time, 0)
+	for _, param := range input.Params {
+		if param.Timestamp != 0 {
+			times = append(times, time.Unix(param.Timestamp, 0))
+		}
+	}
+	return times
+}
+
+func generateRoiDistribution(perUserRoiInDays map[string]int64) []ValueFrequencyPercentage {
+	RoiDayDistribution := make(map[int64]int64)
+	totalCount := float64(0)
+	for _, days := range perUserRoiInDays {
+		if days < 1 {
+			continue
+		}
+		RoiDayDistribution[days] += 1
+		totalCount += 1
+	}
+	daysPercentageDistribution := make(map[int64]float64)
+	for days, count := range RoiDayDistribution {
+		daysPercentageDistribution[days] = float64(count) / totalCount
+	}
+	result := make([]ValueFrequencyPercentage, len(daysPercentageDistribution))
+	idx := 0
+	for value, frequencyPercentage := range daysPercentageDistribution {
+		result[idx] = ValueFrequencyPercentage{
+			Value:               value,
+			FrequencyPercentage: frequencyPercentage,
+		}
+		idx += 1
+	}
+	sort.Slice(result, func(i, j int) bool {
+		return result[i].Value < result[j].Value
+	})
+	return result
+}
+
+func getNewUsers(fromTimeObj time.Time, toTimeObj time.Time, svc s3.S3) map[string]int64 {
+	requestInput :=
+		&s3.GetObjectInput{
+			Bucket: aws.String(userBucketName),
+			Key:    aws.String("per-user-join-time.json"),
+		}
+	result, err := svc.GetObject(requestInput)
+	if err != nil {
+		exitErrorf("Unable to get object, %v", err)
+	}
+	body, err := ioutil.ReadAll(result.Body)
+	if err != nil {
+		exitErrorf("Unable to read body, %v", err)
+	}
+
+	m := map[string]map[string]string{}
+	err = json.Unmarshal(body, &m)
+	if err != nil {
+		//log.Printf("body: %s", fmt.Sprintf("%s", body))
+		exitErrorf("Unable to unmarshall user meta info, %v", err)
+	}
+
+	newUsers := map[string]int64{}
+	for address, userMetaInfo := range m {
+		timestamp, _ := strconv.Atoi(userMetaInfo["timestamp"])
+		userJoinTimestampObj := time.Unix(int64(timestamp), 0)
+		if userJoinTimestampObj.Before(fromTimeObj) || userJoinTimestampObj.After(toTimeObj) {
+			continue
+		}
+		newUsers[address] = int64(timestamp)
+	}
+	return newUsers
+}
+
+func getPriceHistory(tokenName string, fromTimeObj time.Time, toTimeObj time.Time, svc s3.S3) PriceHistory {
+	requestInput :=
+		&s3.GetObjectInput{
+			Bucket: aws.String(priceBucketName),
+			Key:    aws.String("sea-token-price-history.json"),
+		}
+	result, err := svc.GetObject(requestInput)
+	if err != nil {
+		exitErrorf("Unable to get object, %v", err)
+	}
+	body, err := ioutil.ReadAll(result.Body)
+	if err != nil {
+		exitErrorf("Unable to read body, %v", err)
+	}
+
+	priceHistory := PriceHistory{}
+	err = json.Unmarshal(body, &priceHistory)
+	if err != nil {
+		//log.Printf("body: %s", fmt.Sprintf("%s", body))
+		exitErrorf("Unable to unmarshall user meta info, %v", err)
+	}
+
+	return priceHistory
+}
+
+func getPerPayerTransfers(transfers []Transfer) map[string][]Transfer {
+	perUserTransfers := map[string][]Transfer{}
+	for _, transfer := range transfers {
+		if _, ok := perUserTransfers[transfer.FromAddress]; ok {
+			perUserTransfers[transfer.FromAddress] = append(perUserTransfers[transfer.FromAddress], transfer)
+		} else {
+			perUserTransfers[transfer.FromAddress] = make([]Transfer, 0)
+		}
+	}
+	return perUserTransfers
 }
