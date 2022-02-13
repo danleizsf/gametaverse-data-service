758d70bceff2c2b80db4ebef41f1037dff445e56
wy90021 <wy90021@gmail.com>
2022-02-12 19:12:54 -0800

    update

--- get_user_active_dates.go
+++ get_user_active_dates.go
@@ -1,65 +1,52 @@
 package main
 
 import (
-	"io/ioutil"
-	"log"
 	"sort"
-	"strconv"
-	"strings"
 	"time"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 func GetUserActiveDates(fromTimeObj time.Time, toTimeObj time.Time) []UserActivity {
-	sess, _ := session.NewSession(&aws.Config{
-		Region: aws.String("us-west-1"),
-	})
 
-	svc := s3.New(sess)
+	totalTransfers := GetTransfers(fromTimeObj, toTimeObj)
+	//for _, item := range resp.Contents {
+	//	log.Printf("file name: %s\n", *item.Key)
+	//	timestamp, _ := strconv.ParseInt(strings.Split(*item.Key, "-")[0], 10, 64)
+	//	timeObj := time.Unix(timestamp, 0)
+	//	if timeObj.Before(fromTimeObj) || timeObj.After(toTimeObj) {
+	//		continue
+	//	}
 
-	resp, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(dailyTransferBucketName)})
-	if err != nil {
-		exitErrorf("Unable to list object, %v", err)
-	}
-
-	totalTransfers := make([]Transfer, 0)
-	for _, item := range resp.Contents {
-		log.Printf("file name: %s\n", *item.Key)
-		timestamp, _ := strconv.ParseInt(strings.Split(*item.Key, "-")[0], 10, 64)
-		timeObj := time.Unix(timestamp, 0)
-		if timeObj.Before(fromTimeObj) || timeObj.After(toTimeObj) {
-			continue
-		}
-
-		requestInput :=
-			&s3.GetObjectInput{
-				Bucket: aws.String(dailyTransferBucketName),
-				Key:    aws.String(*item.Key),
-			}
-		result, err := svc.GetObject(requestInput)
-		if err != nil {
-			exitErrorf("Unable to get object, %v", err)
-		}
-		body, err := ioutil.ReadAll(result.Body)
-		if err != nil {
-			exitErrorf("Unable to get body, %v", err)
-		}
-		bodyString := string(body)
-		//transactions := converCsvStringToTransactionStructs(bodyString)
-		transfers := ConvertCsvStringToTransferStructs(bodyString)
-		log.Printf("transfer num: %d", len(transfers))
-		//dateString := time.Unix(int64(dateTimestamp), 0).UTC().Format("2006-January-01")
-		totalTransfers = append(totalTransfers, transfers...)
-	}
+	//	requestInput :=
+	//		&s3.GetObjectInput{
+	//			Bucket: aws.String(dailyTransferBucketName),
+	//			Key:    aws.String(*item.Key),
+	//		}
+	//	result, err := svc.GetObject(requestInput)
+	//	if err != nil {
+	//		exitErrorf("Unable to get object, %v", err)
+	//	}
+	//	body, err := ioutil.ReadAll(result.Body)
+	//	if err != nil {
+	//		exitErrorf("Unable to get body, %v", err)
+	//	}
+	//	bodyString := string(body)
+	//	//transactions := converCsvStringToTransactionStructs(bodyString)
+	//	transfers := ConvertCsvStringToTransferStructs(bodyString)
+	//	log.Printf("transfer num: %d", len(transfers))
+	//	//dateString := time.Unix(int64(dateTimestamp), 0).UTC().Format("2006-January-01")
+	//	totalTransfers = append(totalTransfers, transfers...)
+	//}
 	perUserTransfers := map[string][]Transfer{}
 	for _, transfer := range totalTransfers {
 		if _, ok := perUserTransfers[transfer.FromAddress]; ok {
 			perUserTransfers[transfer.FromAddress] = append(perUserTransfers[transfer.FromAddress], transfer)
 		} else {
 			perUserTransfers[transfer.FromAddress] = make([]Transfer, 0)
+		}
+		if _, ok := perUserTransfers[transfer.ToAddress]; ok {
+			perUserTransfers[transfer.ToAddress] = append(perUserTransfers[transfer.ToAddress], transfer)
+		} else {
+			perUserTransfers[transfer.ToAddress] = make([]Transfer, 0)
 		}
 	}
 	perUserActivities := make([]UserActivity, 0) //len(perUserTransfers))
