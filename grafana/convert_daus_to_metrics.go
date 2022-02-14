cc5693c8ddd9b4b7989e992f188f92767afb6821
wy90021 <wy90021@gmail.com>
2022-02-14 11:57:36 -0800

    update

---
+++ grafana/convert_daus_to_metrics.go
@@ -0,0 +1,40 @@
+package grafana
+
+func ConverDausToMetrics() QueryResponse {
+	return []QueryResponseMetric{
+		{
+			Target: "total_daus",
+			Datapoints: []Datapoint{
+				[]float64{
+					1069,
+					1640995200 * 1000,
+				},
+				[]float64{
+					1104,
+					1641081600 * 1000,
+				},
+				[]float64{
+					1114,
+					1641254400 * 1000,
+				},
+			},
+		},
+		{
+			Target: "new_daus",
+			Datapoints: []Datapoint{
+				[]float64{
+					316,
+					1640995200 * 1000,
+				},
+				[]float64{
+					378,
+					1641081600 * 1000,
+				},
+				[]float64{
+					370,
+					1641254400 * 1000,
+				},
+			},
+		},
+	}
+}
