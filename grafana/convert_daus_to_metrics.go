1cb753a4aabb140620f4cb8193785626af023cc4
wy90021 <wy90021@gmail.com>
2022-02-14 13:35:00 -0800

    update

--- grafana/convert_daus_to_metrics.go
+++ grafana/convert_daus_to_metrics.go
@@ -1,42 +1,25 @@
 package grafana
 
-import "gametaverse-data-service/main"
+import (
+	"gametaverse-data-service/schema"
+)
 
-func ConverDausToMetrics(daus []main.Dau) QueryResponse {
+func ConverDausToMetrics(daus []schema.Dau) QueryResponse {
+
 	return []QueryResponseMetric{
-		{
-			Target: "total_daus",
-			Datapoints: []Datapoint{
-				[]float64{
-					1069,
-					1640995200 * 1000,
-				},
-				[]float64{
-					1104,
-					1641081600 * 1000,
-				},
-				[]float64{
-					1114,
-					1641254400 * 1000,
-				},
-			},
-		},
-		{
-			Target: "new_daus",
-			Datapoints: []Datapoint{
-				[]float64{
-					316,
-					1640995200 * 1000,
-				},
-				[]float64{
-					378,
-					1641081600 * 1000,
-				},
-				[]float64{
-					370,
-					1641254400 * 1000,
-				},
-			},
-		},
+		ConvertTotalActiveUserCountMetrics(daus),
+	}
+}
+
+func ConvertTotalActiveUserCountMetrics(daus []schema.Dau) QueryResponseMetric {
+	datapoints := make([]Datapoint, len(daus))
+	for i, dau := range daus {
+		count := dau.TotalActiveUsers.TotalUserCount
+		timestamp := dau.DateTimestamp * 1000
+		datapoints[i] = []float64{float64(count), float64(timestamp)}
+	}
+	return QueryResponseMetric{
+		Target:     "totalActiveUserCount",
+		Datapoints: datapoints,
 	}
 }
