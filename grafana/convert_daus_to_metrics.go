e501792adfb04e868349c5758033d4f3b0a462c1
wy90021 <wy90021@gmail.com>
2022-02-14 13:52:12 -0800

    update

--- grafana/convert_daus_to_metrics.go
+++ grafana/convert_daus_to_metrics.go
@@ -5,35 +5,45 @@ import (
 )
 
 func GetDauMetrics(daus []schema.Dau) QueryResponse {
-
+	newRenteeDatapoints := make([]Datapoint, len(daus))
+	totalRenteeDatapoints := make([]Datapoint, len(daus))
+	newPurchaserDatapoints := make([]Datapoint, len(daus))
+	totalPurchaserDatapoints := make([]Datapoint, len(daus))
+	newUserDatapoints := make([]Datapoint, len(daus))
+	totalUserDatapoints := make([]Datapoint, len(daus))
+	for i, dau := range daus {
+		timestamp := dau.DateTimestamp * 1000
+		newRenteeDatapoints[i] = []float64{float64(dau.NewActiveUsers.PayerCount.RenteeCount), float64(timestamp)}
+		totalRenteeDatapoints[i] = []float64{float64(dau.TotalActiveUsers.PayerCount.RenteeCount), float64(timestamp)}
+		newPurchaserDatapoints[i] = []float64{float64(dau.NewActiveUsers.PayerCount.PurchaserCount), float64(timestamp)}
+		totalPurchaserDatapoints[i] = []float64{float64(dau.TotalActiveUsers.PayerCount.PurchaserCount), float64(timestamp)}
+		newUserDatapoints[i] = []float64{float64(dau.NewActiveUsers.TotalUserCount), float64(timestamp)}
+		totalUserDatapoints[i] = []float64{float64(dau.TotalActiveUsers.TotalUserCount), float64(timestamp)}
+	}
 	return []QueryResponseMetric{
-		GetTotalActiveUserCountMetrics(daus),
-		GetNewActiveUserCountMetrics(daus),
-	}
-}
-
-func GetTotalActiveUserCountMetrics(daus []schema.Dau) QueryResponseMetric {
-	datapoints := make([]Datapoint, len(daus))
-	for i, dau := range daus {
-		count := dau.TotalActiveUsers.TotalUserCount
-		timestamp := dau.DateTimestamp * 1000
-		datapoints[i] = []float64{float64(count), float64(timestamp)}
-	}
-	return QueryResponseMetric{
-		Target:     "totalActiveUserCount",
-		Datapoints: datapoints,
-	}
-}
-
-func GetNewActiveUserCountMetrics(daus []schema.Dau) QueryResponseMetric {
-	datapoints := make([]Datapoint, len(daus))
-	for i, dau := range daus {
-		count := dau.NewActiveUsers.TotalUserCount
-		timestamp := dau.DateTimestamp * 1000
-		datapoints[i] = []float64{float64(count), float64(timestamp)}
-	}
-	return QueryResponseMetric{
-		Target:     "newActiveUserCount",
-		Datapoints: datapoints,
+		{
+			Target:     "newActiveRenteeCount",
+			Datapoints: newRenteeDatapoints,
+		},
+		{
+			Target:     "totalActiveRenteeCount",
+			Datapoints: totalRenteeDatapoints,
+		},
+		{
+			Target:     "newActivePurchaserCount",
+			Datapoints: newPurchaserDatapoints,
+		},
+		{
+			Target:     "totalActivePurchaserCount",
+			Datapoints: totalPurchaserDatapoints,
+		},
+		{
+			Target:     "newActiveUserCount",
+			Datapoints: newUserDatapoints,
+		},
+		{
+			Target:     "totalActiveUserCount",
+			Datapoints: totalUserDatapoints,
+		},
 	}
 }
