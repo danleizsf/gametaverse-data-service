78ad49651f8e9f245a62e55a22e4682be6e8a1c8
wy90021 <wy90021@gmail.com>
2022-02-13 21:59:08 -0800

    update

--- main.go
+++ main.go
@@ -28,31 +28,31 @@ func init() {
 func process(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	input := Input{}
 	json.Unmarshal([]byte(request.Body), &input)
-	log.Printf("path: %s, body: %s", request.Path, request.Body)
+	log.Printf("path: %s, body: %s, httpmethod: %s", request.Path, request.Body, request.HTTPMethod)
 	log.Printf("request: %v", request)
 	if request.Path == "/grafana/search" {
 		response := grafana.Search()
 		return GenerateResponse(response)
 	}
-	if input.Method == "getDaus" {
+	if request.HTTPMethod == "getDaus" {
 		response := GetGameDaus(generateTimeObjs(input))
 		return GenerateResponse(response)
-	} else if input.Method == "getDailyTransactionVolumes" {
+	} else if request.HTTPMethod == "getDailyTransactionVolumes" {
 		response := GetGameDailyTransactionVolumes(generateTimeObjs(input))
 		return GenerateResponse(response)
 		//} else if input.Method == "getUserData" {
 		//	response := getUserData(input.Params[0].Address)
 		//	return GenerateResponse(response)
-	} else if input.Method == "getUserRetentionRate" {
+	} else if request.HTTPMethod == "getUserRetentionRate" {
 		response := GetUserRetentionRate(time.Unix(input.Params[0].FromTimestamp, 0), time.Unix(input.Params[0].ToTimestamp, 0))
 		return GenerateResponse(response)
-	} else if input.Method == "getUserRepurchaseRate" {
+	} else if request.HTTPMethod == "getUserRepurchaseRate" {
 		response := GetUserRepurchaseRate(time.Unix(input.Params[0].FromTimestamp, 0), time.Unix(input.Params[0].ToTimestamp, 0))
 		return GenerateResponse(response)
-	} else if input.Method == "getUserSpendingDistribution" {
+	} else if request.HTTPMethod == "getUserSpendingDistribution" {
 		response := GetUserSpendingDistribution(time.Unix(input.Params[0].FromTimestamp, 0), time.Unix(input.Params[0].ToTimestamp, 0))
 		return GenerateResponse(response)
-	} else if input.Method == "getUserProfitDistribution" {
+	} else if request.HTTPMethod == "getUserProfitDistribution" {
 		userAddressMap := map[string]bool{}
 		for _, param := range input.Params {
 			userAddressMap[param.Address] = true
@@ -60,24 +60,24 @@ func process(ctx context.Context, request events.APIGatewayProxyRequest) (events
 		response := GetUserProfitDistribution(userAddressMap)
 		return GenerateResponse(response)
 		//return generateJsonResponse(response)
-	} else if input.Method == "getUserRoi" {
+	} else if request.HTTPMethod == "getUserRoi" {
 		response := GetUserRoi(time.Unix(input.Params[0].FromTimestamp, 0), time.Unix(input.Params[0].ToTimestamp, 0))
 		return GenerateResponse(response)
 		//return generateJsonResponse(response)
-	} else if input.Method == "getUserActiveDates" {
+	} else if request.HTTPMethod == "getUserActiveDates" {
 		response := GetUserActiveDates(starSharksStartingDate, time.Now(), input.Params[0].Limit)
 		return GenerateResponse(response)
 		//return generateJsonResponse(response)
-	} else if input.Method == "getNewUserProfitableRate" {
+	} else if request.HTTPMethod == "getNewUserProfitableRate" {
 		response := GetNewUserProfitableRate(time.Unix(input.Params[0].FromTimestamp, 0), time.Now(), false)
 		return GenerateResponse(response)
-	} else if input.Method == "getNewUserProfitableRateDebug" {
+	} else if request.HTTPMethod == "getNewUserProfitableRateDebug" {
 		response := GetNewUserProfitableRate(time.Unix(input.Params[0].FromTimestamp, 0), time.Unix(input.Params[0].ToTimestamp, 0), true)
 		return GenerateResponse(response)
-	} else if input.Method == "getUserType" {
+	} else if request.HTTPMethod == "getUserType" {
 		response := GetUserType()
 		return GenerateResponse(response)
-	} else if input.Method == "test" {
+	} else if request.HTTPMethod == "test" {
 		tableNames, _ := dynamoDBClient.ListTables(nil)
 		return GenerateResponse(tableNames.TableNames)
 	}
