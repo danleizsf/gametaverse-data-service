26cdd0f31c43f1a4c3bdac372c2c187876998816
wy90021 <wy90021@gmail.com>
2022-02-14 13:36:33 -0800

    update

--- grafana/convert_daus_to_metrics.go
+++ grafana/convert_daus_to_metrics.go
@@ -23,3 +23,16 @@ func ConvertTotalActiveUserCountMetrics(daus []schema.Dau) QueryResponseMetric {
 		Datapoints: datapoints,
 	}
 }
+
+func ConvertNewActiveUserCountMetrics(daus []schema.Dau) QueryResponseMetric {
+	datapoints := make([]Datapoint, len(daus))
+	for i, dau := range daus {
+		count := dau.NewActiveUsers.TotalUserCount
+		timestamp := dau.DateTimestamp * 1000
+		datapoints[i] = []float64{float64(count), float64(timestamp)}
+	}
+	return QueryResponseMetric{
+		Target:     "newActiveUserCount",
+		Datapoints: datapoints,
+	}
+}
