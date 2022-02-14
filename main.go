f885cb861f7b35e463cb97d8d40111ac58fb9e9b
wy90021 <wy90021@gmail.com>
2022-02-13 22:04:46 -0800

    update

--- main.go
+++ main.go
@@ -34,25 +34,25 @@ func process(ctx context.Context, request events.APIGatewayProxyRequest) (events
 		response := grafana.Search()
 		return GenerateResponse(response)
 	}
-	if request.HTTPMethod == "getDaus" {
+	if input.Method == "getDaus" {
 		response := GetGameDaus(generateTimeObjs(input))
 		return GenerateResponse(response)
-	} else if request.HTTPMethod == "getDailyTransactionVolumes" {
+	} else if input.Method == "getDailyTransactionVolumes" {
 		response := GetGameDailyTransactionVolumes(generateTimeObjs(input))
 		return GenerateResponse(response)
 		//} else if input.Method == "getUserData" {
 		//	response := getUserData(input.Params[0].Address)
 		//	return GenerateResponse(response)
-	} else if request.HTTPMethod == "getUserRetentionRate" {
+	} else if input.Method == "getUserRetentionRate" {
 		response := GetUserRetentionRate(time.Unix(input.Params[0].FromTimestamp, 0), time.Unix(input.Params[0].ToTimestamp, 0))
 		return GenerateResponse(response)
-	} else if request.HTTPMethod == "getUserRepurchaseRate" {
+	} else if input.Method == "getUserRepurchaseRate" {
 		response := GetUserRepurchaseRate(time.Unix(input.Params[0].FromTimestamp, 0), time.Unix(input.Params[0].ToTimestamp, 0))
 		return GenerateResponse(response)
-	} else if request.HTTPMethod == "getUserSpendingDistribution" {
+	} else if input.Method == "getUserSpendingDistribution" {
 		response := GetUserSpendingDistribution(time.Unix(input.Params[0].FromTimestamp, 0), time.Unix(input.Params[0].ToTimestamp, 0))
 		return GenerateResponse(response)
-	} else if request.HTTPMethod == "getUserProfitDistribution" {
+	} else if input.Method == "getUserProfitDistribution" {
 		userAddressMap := map[string]bool{}
 		for _, param := range input.Params {
 			userAddressMap[param.Address] = true
@@ -60,24 +60,24 @@ func process(ctx context.Context, request events.APIGatewayProxyRequest) (events
 		response := GetUserProfitDistribution(userAddressMap)
 		return GenerateResponse(response)
 		//return generateJsonResponse(response)
-	} else if request.HTTPMethod == "getUserRoi" {
+	} else if input.Method == "getUserRoi" {
 		response := GetUserRoi(time.Unix(input.Params[0].FromTimestamp, 0), time.Unix(input.Params[0].ToTimestamp, 0))
 		return GenerateResponse(response)
 		//return generateJsonResponse(response)
-	} else if request.HTTPMethod == "getUserActiveDates" {
+	} else if input.Method == "getUserActiveDates" {
 		response := GetUserActiveDates(starSharksStartingDate, time.Now(), input.Params[0].Limit)
 		return GenerateResponse(response)
 		//return generateJsonResponse(response)
-	} else if request.HTTPMethod == "getNewUserProfitableRate" {
+	} else if input.Method == "getNewUserProfitableRate" {
 		response := GetNewUserProfitableRate(time.Unix(input.Params[0].FromTimestamp, 0), time.Now(), false)
 		return GenerateResponse(response)
-	} else if request.HTTPMethod == "getNewUserProfitableRateDebug" {
+	} else if input.Method == "getNewUserProfitableRateDebug" {
 		response := GetNewUserProfitableRate(time.Unix(input.Params[0].FromTimestamp, 0), time.Unix(input.Params[0].ToTimestamp, 0), true)
 		return GenerateResponse(response)
-	} else if request.HTTPMethod == "getUserType" {
+	} else if input.Method == "getUserType" {
 		response := GetUserType()
 		return GenerateResponse(response)
-	} else if request.HTTPMethod == "test" {
+	} else if input.Method == "test" {
 		tableNames, _ := dynamoDBClient.ListTables(nil)
 		return GenerateResponse(tableNames.TableNames)
 	}
