43405ef442837744585e81c3e3876cc6afabf537
wy90021 <wy90021@gmail.com>
2022-02-13 21:04:07 -0800

    update

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"gametaverse-data-service/grafana"
 	"log"
+	"time"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -21,59 +25,58 @@ func init() {
 	dynamoDBClient = dynamodb.New(sess)
 }
 
-func process(ctx context.Context, request interface{}) (interface{}, error) {
+func process(ctx context.Context, request events.APIGatewayProxyRequest) (interface{}, error) {
+	input := Input{}
+	json.Unmarshal([]byte(request.Body), &input)
 	log.Printf("request: %v", request)
-
-	//input := Input{}
-	//json.Unmarshal([]byte(events.APIGatewayProxyRequest(request).Body), &input)
-	//if request.Path == "/grafana/search" {
-	//	return grafana.Search(), nil
-	//}
-	//if input.Method == "getDaus" {
-	//	return GetGameDaus(generateTimeObjs(input)), nil
-	//} else if input.Method == "getDailyTransactionVolumes" {
-	//	response := GetGameDailyTransactionVolumes(generateTimeObjs(input))
-	//	return response, nil
-	//} else if input.Method == "getUserData" {
-	//	return getUserData(input.Params[0].Address)
-	//} else if input.Method == "getUserRetentionRate" {
-	//	response := GetUserRetentionRate(time.Unix(input.Params[0].FromTimestamp, 0), time.Unix(input.Params[0].ToTimestamp, 0))
-	//	return response, nil
-	//} else if input.Method == "getUserRepurchaseRate" {
-	//	response := GetUserRepurchaseRate(time.Unix(input.Params[0].FromTimestamp, 0), time.Unix(input.Params[0].ToTimestamp, 0))
-	//	return response, nil
-	//} else if input.Method == "getUserSpendingDistribution" {
-	//	response := GetUserSpendingDistribution(time.Unix(input.Params[0].FromTimestamp, 0), time.Unix(input.Params[0].ToTimestamp, 0))
-	//	return response, nil
-	//} else if input.Method == "getUserProfitDistribution" {
-	//	userAddressMap := map[string]bool{}
-	//	for _, param := range input.Params {
-	//		userAddressMap[param.Address] = true
-	//	}
-	//	response := GetUserProfitDistribution(userAddressMap)
-	//	return response, nil
-	//	//return generateJsonResponse(response)
-	//} else if input.Method == "getUserRoi" {
-	//	response := GetUserRoi(time.Unix(input.Params[0].FromTimestamp, 0), time.Unix(input.Params[0].ToTimestamp, 0))
-	//	return response, nil
-	//	//return generateJsonResponse(response)
-	//} else if input.Method == "getUserActiveDates" {
-	//	response := GetUserActiveDates(starSharksStartingDate, time.Now(), input.Params[0].Limit)
-	//	return response, nil
-	//	//return generateJsonResponse(response)
-	//} else if input.Method == "getNewUserProfitableRate" {
-	//	response := GetNewUserProfitableRate(time.Unix(input.Params[0].FromTimestamp, 0), time.Now(), false)
-	//	return response, nil
-	//} else if input.Method == "getNewUserProfitableRateDebug" {
-	//	response := GetNewUserProfitableRate(time.Unix(input.Params[0].FromTimestamp, 0), time.Unix(input.Params[0].ToTimestamp, 0), true)
-	//	return response, nil
-	//} else if input.Method == "getUserType" {
-	//	response := GetUserType()
-	//	return response, nil
-	//} else if input.Method == "test" {
-	//	tableNames, err := dynamoDBClient.ListTables(nil)
-	//	return tableNames.TableNames, err
-	//}
+	if request.Path == "/grafana/search" {
+		return grafana.Search(), nil
+	}
+	if input.Method == "getDaus" {
+		return GetGameDaus(generateTimeObjs(input)), nil
+	} else if input.Method == "getDailyTransactionVolumes" {
+		response := GetGameDailyTransactionVolumes(generateTimeObjs(input))
+		return response, nil
+	} else if input.Method == "getUserData" {
+		return getUserData(input.Params[0].Address)
+	} else if input.Method == "getUserRetentionRate" {
+		response := GetUserRetentionRate(time.Unix(input.Params[0].FromTimestamp, 0), time.Unix(input.Params[0].ToTimestamp, 0))
+		return response, nil
+	} else if input.Method == "getUserRepurchaseRate" {
+		response := GetUserRepurchaseRate(time.Unix(input.Params[0].FromTimestamp, 0), time.Unix(input.Params[0].ToTimestamp, 0))
+		return response, nil
+	} else if input.Method == "getUserSpendingDistribution" {
+		response := GetUserSpendingDistribution(time.Unix(input.Params[0].FromTimestamp, 0), time.Unix(input.Params[0].ToTimestamp, 0))
+		return response, nil
+	} else if input.Method == "getUserProfitDistribution" {
+		userAddressMap := map[string]bool{}
+		for _, param := range input.Params {
+			userAddressMap[param.Address] = true
+		}
+		response := GetUserProfitDistribution(userAddressMap)
+		return response, nil
+		//return generateJsonResponse(response)
+	} else if input.Method == "getUserRoi" {
+		response := GetUserRoi(time.Unix(input.Params[0].FromTimestamp, 0), time.Unix(input.Params[0].ToTimestamp, 0))
+		return response, nil
+		//return generateJsonResponse(response)
+	} else if input.Method == "getUserActiveDates" {
+		response := GetUserActiveDates(starSharksStartingDate, time.Now(), input.Params[0].Limit)
+		return response, nil
+		//return generateJsonResponse(response)
+	} else if input.Method == "getNewUserProfitableRate" {
+		response := GetNewUserProfitableRate(time.Unix(input.Params[0].FromTimestamp, 0), time.Now(), false)
+		return response, nil
+	} else if input.Method == "getNewUserProfitableRateDebug" {
+		response := GetNewUserProfitableRate(time.Unix(input.Params[0].FromTimestamp, 0), time.Unix(input.Params[0].ToTimestamp, 0), true)
+		return response, nil
+	} else if input.Method == "getUserType" {
+		response := GetUserType()
+		return response, nil
+	} else if input.Method == "test" {
+		tableNames, err := dynamoDBClient.ListTables(nil)
+		return tableNames.TableNames, err
+	}
 	return "", nil
 }
 
