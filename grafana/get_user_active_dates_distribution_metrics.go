9e6c1f45b91623b06f94e322b80d04787b8162fb
wy90021 <wy90021@gmail.com>
2022-02-15 12:55:44 -0800

    update

--- grafana/get_user_active_dates_distribution_metrics.go
+++ grafana/get_user_active_dates_distribution_metrics.go
@@ -2,11 +2,15 @@ package grafana
 
 import (
 	"gametaverse-data-service/schema"
+	"log"
 )
 
 func GetUserActualActiveDatesDistributionMetrics(userActivities []schema.UserActivity) QueryResponse {
 	actualActiveDaysDistributionDatapoints := make([]Datapoint, len(userActivities))
 	for i, userActivity := range userActivities {
+		if userActivity.TotalDatesCount < userActivity.ActiveDatesCount {
+			log.Printf("wrong user active dates is wrong, total %d, actual %d", userActivity.TotalDatesCount, userActivity.ActiveDatesCount)
+		}
 		actualActiveDaysDistributionDatapoints[i] = []float64{float64(userActivity.ActiveDatesCount), 0}
 	}
 	return []QueryResponseMetric{
