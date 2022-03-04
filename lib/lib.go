83e426cec934c25b8a5dc20f57698094a9b18f9c
danlei <danleizhousf@gmail.com>
2022-03-03 23:41:13 -0800

    commit

--- lib/lib.go
+++ lib/lib.go
@@ -45,39 +45,53 @@ func GetSummary(s3client *s3.S3, date string) schema.Summary {
 }
 
 func GetUserActionsRange(s3client *s3.S3, timestampA int64, timestampB int64) map[string][]schema.UserAction {
+
 	start := time.Unix(timestampA, 0)
 	end := time.Unix(timestampB, 0)
 	length := 0
 	for d := start; !d.After(end); d = d.AddDate(0, 0, 1) {
 		length++
 	}
-	useractions := make(map[string][]schema.UserAction, 0)
+	useractionsByDate := make([]map[string][]schema.UserAction, 0)
 	var wg sync.WaitGroup
 	wg.Add(length)
+	i := 0
 	for d := start; !d.After(end); d = d.AddDate(0, 0, 1) {
 		go func(s3client *s3.S3, d time.Time) {
 			defer wg.Done()
 			date := d.Format(schema.DateFormat)
 			ua := GetUserActions(s3client, date)
-			for user, actions := range ua {
-				actionsWithDate := make([]schema.UserAction, len(actions))
-				for i, a := range actions {
-					actionsWithDate[i] = schema.UserAction{
-						Value:  a.Value,
-						Date:   date,
-						Action: a.Action,
-					}
-				}
-
-				if existingActions, exists := useractions[user]; exists {
-					useractions[user] = append(existingActions, actionsWithDate...)
-				} else {
-					useractions[user] = actionsWithDate
-				}
-			}
+			useractionsByDate[i] = ua
 		}(s3client, d)
+		i++
 	}
 	wg.Wait()
+
+	j := 0
+	useractions := make(map[string][]schema.UserAction, 0)
+
+	for d := start; !d.After(end); d = d.AddDate(0, 0, 1) {
+		ua := useractionsByDate[j]
+		date := d.Format(schema.DateFormat)
+		for user, actions := range ua {
+			actionsWithDate := make([]schema.UserAction, len(actions))
+			for i, a := range actions {
+				actionsWithDate[i] = schema.UserAction{
+					Value:  a.Value,
+					Date:   date,
+					Action: a.Action,
+				}
+			}
+
+			if existingActions, exists := useractions[user]; exists {
+				useractions[user] = append(existingActions, actionsWithDate...)
+			} else {
+				useractions[user] = actionsWithDate
+			}
+		}
+		j++
+	}
+
 	return useractions
 }
 
