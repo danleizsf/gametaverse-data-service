ad6abcbd9aa90697c20aa36574630be3c45c0e05
danlei <danleizhousf@gmail.com>
2022-03-05 00:37:24 -0800

    add cacche

---
+++ lib/cache.go
@@ -0,0 +1,31 @@
+package lib
+
+import (
+	"gametaverse-data-service/schema"
+	"sync"
+)
+
+type Cache struct {
+	mux *sync.Mutex
+	UA  map[string]map[string][]schema.UserAction
+}
+
+func NewCache() *Cache {
+	mux := &sync.Mutex{}
+	return &Cache{
+		mux: mux,
+	}
+}
+
+func (c *Cache) Add(key string, ua map[string][]schema.UserAction) {
+	c.mux.Lock()
+	c.UA[key] = ua
+	c.mux.Unlock()
+}
+
+func (c *Cache) Get(key string) (map[string][]schema.UserAction, bool) {
+	c.mux.Lock()
+	res, exists := c.UA[key]
+	c.mux.Unlock()
+	return res, exists
+}
