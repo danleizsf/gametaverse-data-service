6f986815cacf9048c3df93988238022e1573e80e
danlei <danleizhousf@gmail.com>
2022-03-21 23:48:10 -0700

    commit

--- grafana/search.go
+++ grafana/search.go
@@ -2,40 +2,6 @@ package grafana
 
 func Search() []string {
 	return []string{
-		"daus",
-		"daily_transaction_volume",
-		"new_user_profitable_rate",
-		"user_repurchase_rate",
-
-		"user_actual_active_dates_distribution",
-		"user_total_active_dates_distribution",
-
-		"new_user_spending_usd_distribution",
-		"new_rentee_spending_usd_distribution",
-		"new_purchaser_spending_usd_distribution",
-		"new_hybrider_spending_usd_distribution",
-		"new_rentee_spending_token_distribution",
-		"new_purchaser_spending_token_distribution",
-		"new_hybrider_spending_token_distribution",
-
-		"new_user_profit_usd_distribution",
-		"new_rentee_profit_usd_distribution",
-		"new_purchaser_profit_usd_distribution",
-		"new_hybrider_profit_usd_distribution",
-		"new_rentee_profit_token_distribution",
-		"new_purchaser_profit_token_distribution",
-		"new_hybrider_profit_token_distribution",
-
-		"new_user_type",
-
-		"new_user_profitable_days",
-		"new_rentee_profitable_days",
-		"new_purchaser_profitable_days",
-		"new_hybrider_profitable_days",
-		"whale_sort_by_gain",
-		"whale_sort_by_profit",
-		"whale_sort_by_spending",
-
 		"daus2",
 		"daily_transaction_volume2",
 		"new_user_profitable_rate2",
